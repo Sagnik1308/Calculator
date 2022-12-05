57aa93bd9df7af8b6748b149ed24b273db217574
Sagnik <csagnik13@gmail.com>
2022-12-06 02:02:29 +0530

    Added all JavaScript features for operations by mouseclick

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ function subtract(a,b){
     return a-b;
 }
 function multiply(a,b){
-    return a*b;
+    if (parseFloat(a)!=parseInt(a) || parseFloat(b)!=parseInt(b)){
+        return (a*b).toFixed(2);
+    }else{
+        return a*b;
+    }
 }
 function divide(a,b){
-    return a/b;
+    if (b==0) return 'Infinity';
+    return (a%b==0)?(a/b):(a/b).toFixed(2);
+}
+function modulo(a,b){
+    return a%b;
 }
 function operate(operator,x,y){
+    if (parseInt(x)==parseFloat(x)){
+        x=parseInt(x);
+    }else{
+        x=parseFloat(x);
+    }
+    if (parseInt(y)==parseFloat(y)){
+        y=parseInt(y);
+    }else{
+        y=parseFloat(y);
+    }
     if (operator=='+'){
         return add(x,y);
     }else if (operator=='-'){
@@ -19,36 +37,75 @@ function operate(operator,x,y){
         return multiply(x,y);
     }else if (operator=='/'){
         return divide(x,y);
+    }else if (operator=='%'){
+        return modulo(a,b);
     }
 }
-let display="",result=0,x=0,y=0,op='';
+let display="",op='',op1='',op2='';
 const outWindow=document.querySelector('.outer-display');
 const innerWindow=document.querySelector('.inner-display');
 const btns=document.querySelectorAll('button');
 btns.forEach(btn=>btn.addEventListener('click',function evaluate(e){
     const ch=e.target.value;
-    if (ch!='=') display+=ch;
-    outWindow.textContent=display;
+    if (ch=='.' && op1==''){
+        display='0.';
+        op1='0';
+        outWindow.textContent='0.';
+    }else if (ch=='0' && op1==''){
+        display='0';
+        outWindow.textContent='0';
+    }else if (ch!='=' && ch!='C'){
+        display+=ch;
+        outWindow.textContent=display;
+    }
     if (ch>='0' && ch<='9'){
-        result+=parseInt(ch);
-        if (x==0){
-            x=result;
+        if (op1=='' || op==''){
+            op1+=ch;
         }else{
-            y=parseInt(ch);
+            op2+=ch;
         }
     }else if (ch=='='){
-        result=operate(op,x,y);
-        innerWindow.textContent=result;
-        x=result;
-        y=0;
-        result=0;
-    }else{
-        if (y!=0){
-            result=operate(op,x,y);
-            innerWindow.textContent=result;
-            x=result;
-            y=0;
-            result=0;
+        op1=operate(op,op1,op2)+'';
+        innerWindow.textContent=op1;
+        op2='';
+        op='';
+    }else if (ch=='AC'){
+        display='0';
+        op1='';
+        op2='';
+        op='';
+        outWindow.textContent='0';
+        innerWindow.textContent='';
+    }else if (ch=='.'){
+        if (op1!='' && op==''){
+            op1+=ch;
+        }else{
+            op2+=ch;
+        }
+    }else if (ch=='C'){
+        if (op1!='' && op2=='' && op==''){
+            display='0';
+            op1='';
+            op2='';
+            op='';
+            innerWindow.textContent='';
+        }else if (op2!=''){
+            display=display.substring(0,display.length-1);
+            op2=op2.substring(0,op2.length-1);
+        }else if (op!=''){
+            display=display.substring(0,display.length-1);
+            op=op.substring(0,op.length-1);
+        }else{
+            display=display.substring(0,display.length-1);
+            op1=op1.substring(0,op1.length-1);
+        }
+        outWindow.textContent=display;
+    }
+    else{
+        if (op2!=''){
+            op1=operate(op,op1,op2)+'';
+            innerWindow.textContent=op1;
+            op2='';
         }
         op=ch;
     }
